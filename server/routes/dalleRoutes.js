73cd7cb902716b52da44e2323ef8a75b4e1df7f5
Hasan <65603504+hsnyrdkl@users.noreply.github.com>
2023-05-29 03:10:47 +0300

    Update dalleRoutes.js

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -13,24 +13,27 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 router.route("/").get((req, res) => {
-  res.send("Hello from OpenAI");
+  res.status(200).json({ message: "Hello from DALL-E!" });
 });
 
 router.route("/").post(async (req, res) => {
   try {
     const { prompt } = req.body;
+
     const aiResponse = await openai.createImage({
       prompt,
       n: 1,
       size: "1024x1024",
       response_format: "b64_json",
     });
-    const image = aiResponse.data.data[0].b64_json;
 
+    const image = aiResponse.data.data[0].b64_json;
     res.status(200).json({ photo: image });
   } catch (error) {
-    console.log(error);
-    res.status(500).send(error);
+    console.error(error);
+    res
+      .status(500)
+      .send(error?.response.data.error.message || "Something went wrong");
   }
 });
 
